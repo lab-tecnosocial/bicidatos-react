d3e93ea48e2e8fea43ded8e133480b08baddfa03
Heidi2018 <heidihuancah@gmail.com>
2023-04-24 01:11:17 -0400

    update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import { auth, provider } from "../../database/firebase";
+import Logo from '../../assets/images/logo.png';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import HomeIcon from '@material-ui/icons/Home';
@@ -16,15 +20,51 @@ const useStyles = makeStyles((theme) => ({
     marginRight: theme.spacing(2),
   },
   title: {
-    flexGrow: 1
+    flexGrow: 1,
+    textAlign:'center',
   },
   header: {
     backgroundColor: '#15C0EA',
-  }
+  },
+  signInWithGoogle:{
+
+  },
+signOut:{
+  
+}
 }));
+const signInWithGoogle = async () => {
+  try {
+    await auth.signInWithPopup(provider)
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+const signOut = async () => {
+  auth.signOut();
+}
+const [user,setUser]=useState(null);
+  if(user){
+     // console.log(user);
+  } else {
+    // console.log(null);
+  }
+  useEffect(() => {
+    auth.onAuthStateChanged(persona => {
+      if (persona) {
+        setUser(persona);
+      } else {
+        setUser(null);
+      }
+    });
+  })
 
 export default function Header() {
   const classes = useStyles();
+
+  
 
   return (
     <div className={classes.root}>
@@ -35,8 +75,14 @@ export default function Header() {
               <HomeIcon />
             </IconButton>
             <Typography variant="h6" className={classes.title} >
-              BiciDatos
+              <img src={Logo} width='80px' height='60 px'/>
             </Typography>
+
+            {
+            user ? <Button  onClick={signOut} className={classes.signOut} variant="text" endIcon={<AccountCircleIcon />} >Cerrar sesión</Button> :
+              <Button  onClick={signInWithGoogle} className={classes.signInWithGoogle} variant="text" endIcon={<ExitToAppIcon />} >Iniciar sesión</Button>
+            }
+
           </Toolbar>
         </Container>
       </AppBar>
