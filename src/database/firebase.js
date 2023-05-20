7bfcaa8edf334e2f05e5b91e5446c505d8880147
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2023-05-20 09:29:58 -0400

    add: vite configuration

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -3,14 +3,14 @@ import "firebase/firestore";
 import "firebase/storage";
 import "firebase/auth";
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId:  process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId:  import.meta.env.VITE_MEASUREMENT_ID
 };
   
 firebase.initializeApp(firebaseConfig);
